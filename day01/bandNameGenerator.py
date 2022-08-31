7951797337c358877e95425272b82dd7ad801972
Thiago Wilton <thiagowfn@gmail.com>
2022-08-31 15:36:15 -0300

    Primeiro dia de projeto

---
+++ day01/bandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!\n")
+
+cityName = input("What's name of the city you grew up in?\n")
+
+petName = input("\nWhat's your pet's name?\n")
+
+print(f'\nYour band name could be {cityName} {petName}')
