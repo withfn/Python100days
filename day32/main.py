642c53b50c3a4a9bc84a5cad4dd32571b5a73c9e
Thiago <thiagowfn@gmail.com>
2022-09-13 17:02:06 -0300

    day32 update

--- day32/main.py
+++ day32/main.py
@@ -1,13 +1,29 @@
-##################### Extra Hard Starting Project ######################
+import smtplib
+import pandas as pd
+import datetime as dt
+from random import randint
 
-# 1. Update the birthdays.csv
+#login
+my_email = "withfnas@gmail.com"
+password = "vbddeofxkejhbgnb"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+#date
+dt = dt.datetime.today()
+day = dt.day
+month = dt.month
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+#open csv birthdays
+df = pd.read_csv("birthdays.csv")
+birthdays = df.to_dict(orient="records")
 
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
+#send email if has a birthday today
+for person in birthdays:
+    if person['day'] == day and person['month'] == month:
+        with open(f"letter_templates/letter_{randint(1, 3)}.txt", 'r') as letter:
+            sample_letter = letter.read()
+            new_letter = sample_letter.replace('[NAME]', person['name'])
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=person['email'], 
+                                msg=f"Subject:Happy birthday!\n\n{letter}")
