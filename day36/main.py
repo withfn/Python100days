27f7f9223c1cb7e3db0773865e9ba0d1e7a7fe8b
Thiago <thiagowfn@gmail.com>
2022-09-15 18:03:26 -0300

    day36 update

--- day36/main.py
+++ day36/main.py
@@ -1,7 +1,12 @@
 import requests
+from datetime import date, timedelta
+from newsapi import NewsApiClient
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+def get_percentage():
+    return round(float(yesterday_stock) - float(before_yesterday_stock) / float(before_yesterday_stock) * 100)
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -14,13 +19,23 @@ alphavantage_parameters = {
 tesla = requests.get(url="https://www.alphavantage.co/query", params=alphavantage_parameters)
 print(tesla.status_code)
 tesla_data = tesla.json()
-yesterday_stock = tesla_data["Time Series (Daily)"]["2022-09-14"]
-before_yesterday_stock = tesla_data["Time Series (Daily)"]["2022-09-13"]
-print(yesterday_stock, before_yesterday_stock)
 
+today = date.today()
+yesterday_date = (today - timedelta(days=1))
+before_yesterday = (today - timedelta(days=2))
+yesterday_stock = tesla_data["Time Series (Daily)"][str(yesterday_date)]["4. close"]
+before_yesterday_stock = tesla_data["Time Series (Daily)"][str(before_yesterday)]["4. close"]
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+newsapi = NewsApiClient(api_key="e4d15516139f4a0e91f1520391f7a26f")
+all_articles = newsapi.get_everything(q='tesla',
+                                    sources='bbc-news',
+                                    language='en',
+                                    country='us')
+
+print(all_articles)
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
