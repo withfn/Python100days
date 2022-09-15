c88806f9acc006c13b6f38b8bcda574de979e7fb
Thiago Wilton <thiagowfn@gmail.com>
2022-09-15 08:32:14 -0300

    day35 update

--- day35/main.py
+++ day35/main.py
@@ -1,15 +1,36 @@
 import requests
+from twilio.rest import Client
 
-LATITUDE = -23.525193
-LONGITUDE = -46.823059
+account_sid = "account"
+auth_token = "token"
 
-api_key ="c6a1785b783784e38f080a68032a34f2"
+LATITUDE = '-23.525193'
+LONGITUDE = '-46.823059'
+lat_lon = ','.join([LATITUDE, LONGITUDE])
+api_key ="api"
 
 parameters = {
-    "lat": LATITUDE,
-    "lon": LONGITUDE,
-    "appid": api_key
+    "key": api_key,
+    "q": lat_lon,
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response = requests.get(url="http://api.weatherapi.com/v1/forecast.json", params=parameters)
 print(response.status_code)
+weather_data = response.json()
+weather_slice = weather_data["forecast"]["forecastday"][0]["hour"][0:13]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    if hour_data["chance_of_rain"] >= 70:
+        will_rain = True
+        
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. remember to bring an umbrella☔",
+        from_='',
+        to=''
+    )
+    print(message.status)
