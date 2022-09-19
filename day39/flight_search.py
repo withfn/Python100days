3335fcf85cabf49771d867ec0a5b957b00130d57
Thiago Wilton <thiagowfn@gmail.com>
2022-09-19 09:35:18 -0300

    day39 update

--- day39/flight_search.py
+++ day39/flight_search.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timedelta
+from flight_data import FlightData
 
 API_KEY = "066wYwVtzmgdPLv_pevt15Cw6LN1c0Ic"
 
@@ -13,23 +14,39 @@ class FlightSearch:
         
         
     
-    def get_search(self):
+    def get_search(self, origin_city_code, destination_city_code):
         search_endpoint = f"{self.flight_endpoint}/v2/search"
         parameters = {
-            "fly_from": "LON",
-            "fly_to": "PAR",
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
             "date_from": date_from.strftime("%d/%m/%Y"),
             "date_to": date_to.strftime("%d/%m/%Y"),
             "nights_in_dst_from": 7,
             "nights_in_dst_to": 28,
-            "price_to": 66,
             "flight_type": "round",
             "one_for_city": 1,
             "max_stopovers": 0,
             "curr": "GBP"
         }
-        request = requests.get(url=search_endpoint, params=parameters, headers=self.headers).json()["data"][0]
-        print(request)
+        response = requests.get(url=search_endpoint, params=parameters, headers=self.headers)
+        
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}")
+            return None
+        
+        flight_data =FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
         
     def get_iataCode(self, place):
         config = { "term": place }
--- day39/main.py
+++ day39/main.py
@@ -2,4 +2,12 @@ from data_manager import DataManager
 
 dataManager = DataManager()
 
-# dataManager.verify_iata_code()
+ORIGIN_CITY_IATA = "LON"
+
+#dataManager.verify_iata_code()
+
+for destination in dataManager.places:
+    flight = dataManager.get_search(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+    )
