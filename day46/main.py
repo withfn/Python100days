7aded0f8f5aff160ca5dfb94def0fc40e06798aa
Thiago <thiagowfn@gmail.com>
2022-09-29 16:10:13 -0300

    day 46 update

--- day46/main.py
+++ day46/main.py
@@ -1,11 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 
-clientId = ""
-clientSecret = ""
 
-year = input("Which year do you want to travel to? type date in this format YYYY-MM-DD: ")
+date = input("Which year do you want to travel to? type date in this format YYYY-MM-DD: ")
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/2001-08-12/{year}").text
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}").text
 
-print(response)
+soup = BeautifulSoup(response, "html.parser")
+
+all_titles = soup.find_all(name="h3", class_="a-no-trucate")
+ 
+titles = [title.getText().strip() for title in all_titles]
+
+############################### Spotify
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+ 
+CLIENT_ID = "xx"
+CLIENT_SECRET = "xx"
+URI = "http://example.com"
+ 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=URI,
+                                               scope="playlist-modify-private",
+                                             ))
+
+# user-library-read
+results = sp.current_user()
+USER_ID = results['id']
+# print(USER_ID) you will need user id to create a playlist and add tracks!
+ 
+uris = [sp.search(title)['tracks']['items'][0]['uri'] for title in titles]
+
+PLAYLIST_ID = sp.user_playlist_create(user=USER_ID,public=False,name=f"{date} BillBoard-100")['id']
+ 
+sp.user_playlist_add_tracks(playlist_id=PLAYLIST_ID,tracks=uris,user=USER_ID)
