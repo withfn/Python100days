2df2e85fb2bcbaa654eeb61ae0928f8f38576a23
Thiago Wilton <thiagowfn@gmail.com>
2022-10-11 17:53:01 -0300

    day48 update

--- day48/main.py
+++ day48/main.py
@@ -4,20 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# chrome_driver_path = "chromedriver"
-# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+chrome_driver_path = "chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-# driver.get("http://orteil.dashnet.org/experiments/cookie/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-# cookie = driver.find_element(By.ID,'cookie')
-# cookie.click()
+cookie = driver.find_element(By.ID,'cookie')
 
-time_execution = 300   # [seconds]
+time_execution = 30   # [seconds]
 initial_time = time.time()
 timeout = initial_time + time_execution
+step = 5 + initial_time
 
+while True:
+    cookie.click()
+    if time.time() > timeout:
+        break
+    if time.time() > step:
+        step += 5
+        print(step)
+        # break
+    
 
-while time.time() != timeout:
-    time_now = 0
-    if time.time() == initial_time + 5:
-        time_now = 
+print(timeout)
